ce576619cdf1dfff278c43d534da2c43b57253de
dhanushmr0211 <1si24cs051>
2026-02-18 12:07:52 +0530

    implemented Word-like text boxes and fixed bugs

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for Electrical Survey App PWA
-const CACHE_NAME = 'electrical-survey-v5.5';
+const CACHE_NAME = 'electrical-survey-v5.9';
 const ASSETS_TO_CACHE = [
   '.',
   './index.html',
